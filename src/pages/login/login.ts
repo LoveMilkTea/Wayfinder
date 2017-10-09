647c64c2e168fc39280da18e93f779e9d683d99c
Your Name <cepugh@hawaii.edu>
2017-10-09 11:57:11 -1000

    Create a new account page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,6 +4,9 @@ import {User} from "../../models/user";
 import {AngularFireAuth} from "angularfire2/auth"
 import {AuthProvider} from "../../providers/auth/auth";
 import {NgForm} from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator } from '../../validators/email';
+import { SignupPage } from '../signup/signup';
 
 /**
  * Generated class for the LoginPage page.
@@ -20,44 +23,51 @@ import {NgForm} from "@angular/forms";
 export class LoginPage {
 
     user = {} as User;
+    loginForm:FormGroup;
     public loading:Loading;
 
-    constructor(public authData: AuthProvider, public alertCtrl: AlertController, private toast: ToastController, public navCtrl: NavController, public navParams: NavParams,  public loadingCtrl: LoadingController) {
+    constructor(public authData: AuthProvider, public alertCtrl: AlertController, private toast: ToastController, public navCtrl: NavController, public navParams: NavParams,  public loadingCtrl: LoadingController, public formBuilder: FormBuilder) {
+        this.loginForm = formBuilder.group({
+            email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+            password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+        });
     }
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad LoginPage');
     }
 
-    login(user: User) {
-        // this is for validation
-        // if (!this.loginForm.valid){
-        //     console.log(this.loginForm.value);
-        // } else {
-        console.log(user);
-        this.authData.loginUser(user.email, user.password)
-            .then(authData => {
-                this.navCtrl.setRoot('AdminPage');
-            }, error => {
-                console.log("never");
-                this.loading.dismiss().then(() => {
-                    let alert = this.alertCtrl.create({
-                        message: `${error.message} Please try again`,
-                        buttons: [
-                            {
-                                text: "Ok",
-                                role: 'cancel'
-                            }
-                        ]
+    loginUser() {
+        if (!this.loginForm.valid) {
+            console.log(this.loginForm.value);
+        } else {
+            this.authData.loginUser(this.loginForm.value.email, this.loginForm.value.password)
+                .then(authData => {
+                    this.navCtrl.setRoot('AdminPage');
+                }, error => {
+                    console.log("never");
+                    this.loading.dismiss().then(() => {
+                        let alert = this.alertCtrl.create({
+                            message: `${error.message} Please try again`,
+                            buttons: [
+                                {
+                                    text: "Ok",
+                                    role: 'cancel'
+                                }
+                            ]
+                        });
+                        alert.present();
                     });
-                    alert.present();
                 });
+            this.loading = this.loadingCtrl.create({
+                dismissOnPageChange: true,
             });
-        this.loading = this.loadingCtrl.create({
-            dismissOnPageChange: true,
-        });
-        this.loading.present();
+            this.loading.present();
+        }
     }
 
+    createAccount() {
+        this.navCtrl.push('SignupPage');
+    }
 }
 
