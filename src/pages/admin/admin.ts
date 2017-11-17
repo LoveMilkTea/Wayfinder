adca5febe00ae1c31a3306ec3ff3602684115a37
Your Name <cepugh@hawaii.edu>
2017-11-17 09:46:05 -1000

    Fixed admin page and edit data page to use database service

--- src/pages/admin/admin.ts
+++ src/pages/admin/admin.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { AngularFireAuth } from "angularfire2/auth";
-import { FIREBASE_CONFIG } from "./../../app.firebase.config";
-import * as firebase from 'firebase';
+import {FirebaseProvider} from "../../providers/firebase/firebase";
 
 @IonicPage()
 @Component({
@@ -10,28 +9,16 @@ import * as firebase from 'firebase';
     templateUrl: 'admin.html',
 })
 export class AdminPage {
-    userInputRef: any;
-    masterDataRef: any;
-    App: any;
-    db: any;
     items: string[];
     filterValue: any;
 
-    constructor(private afAuth: AngularFireAuth, private toast: ToastController, public navCtrl: NavController, public navParams: NavParams) {
-        if (!firebase.apps.length) {
-            this.App = firebase.initializeApp(FIREBASE_CONFIG);
-        } else {
-            this.App = firebase;
-        }
-        this.db = this.App.database();
-        this.userInputRef = this.db.ref('/dataPoints/');
-        this.masterDataRef = this.db.ref('/testPoints');
+    constructor(private afAuth: AngularFireAuth, private toast: ToastController, public navCtrl: NavController, public navParams: NavParams, public database: FirebaseProvider) {
         this.filterValue = 'showAll';
     }
 
     ionViewDidLoad() {
         const item = [];
-        this.userInputRef.once('value').then(function (datakey) {
+        this.database.userInput.once('value').then(function (datakey) {
             datakey.forEach(function (data) {
                 const temp = data.val();
                 Object.assign(temp, {
@@ -50,10 +37,10 @@ export class AdminPage {
     approve(value) { // 'value' is the key for the entry
         console.log(value);
         console.log(value.key);
-        this.userInputRef.child(value.key).update({'status': 'approved'});
-        this.masterDataRef = this.masterDataRef.push();
+        this.database.userInput.child(value.key).update({'status': 'approved'});
+        this.database.masterData = this.database.masterData.push();
         console.log("not broke yet2");
-        this.masterDataRef.set({
+        this.database.masterData.set({
             'name': value.pointName,
             'address': value.address,
             'lat': value.latitude,
@@ -68,7 +55,7 @@ export class AdminPage {
     }
 
     deny(value) {
-        this.userInputRef.child(value.key).update({'status': 'denied'});
+        this.database.userInput.child(value.key).update({'status': 'denied'});
         this.filterItems(this.filterValue);
     }
 
@@ -77,14 +64,14 @@ export class AdminPage {
     }
 
     deleteItem(value) { // 'value' is the key for the entry
-        this.userInputRef.child(value.key).remove();
+        this.database.userInput.child(value.key).remove();
         this.filterItems(this.filterValue); // refresh the page
     }
     filterItems(value){
         this.filterValue = value;
         console.log(this.filterValue);
         const item = [];
-        this.userInputRef.once('value').then(function (datakey) {
+        this.database.userInput.once('value').then(function (datakey) {
             datakey.forEach(function (data) {
                 const temp = data.val();
                 Object.assign(temp, {
